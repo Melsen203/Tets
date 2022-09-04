55f2e9c9b37a745ee37bc56311d85150fe2ffe69
Melsen203 <melensill3d44553@gmail.com>
2022-09-04 10:06:32 +0200

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".items");
+const navMenu = document.querySelector(".hidden-menu");
 
 hamburger.addEventListener ("click", () => {
     hamburger.classList.toggle("active");
