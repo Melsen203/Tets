434bb6ede090b19bd8d743e8a5fb8b0339a568ff
Melsen203 <melensill3d44553@gmail.com>
2022-09-20 19:18:49 +0200

    s

--- js/cap.js
+++
@@ -1,7 +0,0 @@
-const cap = document.querySelector(".cap");
-const chapters = document.querySelector(".chapters");
-
-cap.addEventListener ("click", () => {
-    hamburger.classList.toggle("active");
-    chapters.classList.toggle("active");
-} )
