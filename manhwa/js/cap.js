8ba7e27e83c6f281c937a2b913ea761b9a27704c
Melsen203 <melensill3d44553@gmail.com>
2022-09-24 08:21:08 +0200

    s

---
+++ manhwa/js/cap.js
@@ -0,0 +1,7 @@
+const cap = document.querySelector(".cap");
+const chapters = document.querySelector(".chapters");
+
+cap.addEventListener ("click", () => {
+    hamburger.classList.toggle("active");
+    chapters.classList.toggle("active");
+} )
