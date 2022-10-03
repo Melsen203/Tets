fb9780ad3c01fb576b0d10e73f02dd4baf86fa06
Melsen203 <melensill3d44553@gmail.com>
2022-10-03 16:45:10 +0200

    s

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 6,
     spaceBetween: 20,
-    loop: false,
+    loop: true,
     centerSlide: 'true',
     fade: 'true',
     grabCursor: 'true',
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-      dynamicBullets: true,
+      dynamicBullets: false,
     },
     navigation: {
       nextEl: ".swiper-button-next",
